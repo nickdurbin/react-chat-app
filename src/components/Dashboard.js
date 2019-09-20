69fa97757cb60c3727cd4da609e3a8604680898a
ndurbin <nick.durbin@gmail.com>
2019-09-20 05:20:36 -0400

    Added some basic styling to the chat memeber window and the chat window.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
+// import FaceIcon from '@material-ui/icons/Face';
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: '3rem',
     padding: theme.spacing(3, 2),
+    textAlign: 'center'
   },
   flex: {
     display: 'flex'
   },
   memberList: {
     width: '30%',
-    height: '300px'
+    height: '300px',
+    borderRight: '1px solid black'
   },
   chatWindow: {
     width: '70%',
@@ -41,10 +48,25 @@ function Dashboard() {
         </Typography>
         <div className={classes.flex}>
           <div className={classes.memberList}>
-
+            <List>
+              {
+                ['Gary', 'Robert'].map(member =>
+                  <ListItem key={member} button>
+                    <ListItemText primary="member" />
+                  </ListItem>
+                  )
+              }
+            </List>
           </div>
           <div className={classes.chatWindow}>
-            
+          {
+                [{ from: 'user', msg: 'hello' }].map((chat, index) =>
+                  <div className={classes.flex} key={index} button>
+                    <Chip label={chat.from} className={classes.chip} />
+                    <Typography variant="p">{chat.msg}</Typography>
+                  </div>
+                  )
+              }
           </div>
         </div>
         <div className={classes.flex}>
