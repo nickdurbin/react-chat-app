4a1dbb2e1523f0ae56716126317d74b15812e6cd
ndurbin <nick.durbin@gmail.com>
2019-09-20 06:44:57 -0400

    Set up the reducer in the store with an initial state of data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Dashboard from './components/Dashboard'
+import Store from './store/Store'
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <Store>
+        <Dashboard />
+      </Store>
     </div>
   );
 }
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, {useReducer } from 'react'
 
-const CTX = React.createContext()
+export const CTX = React.createContext()
 
-function Store(props) {
+const intialState = {
+  Gary: [
+    {from: 'Richard', msg: 'Is your property available?'},
+    {from: 'Jim', msg: 'Is it okay to arrive around 4PM?'},
+  ],
+  Michele: [
+    {from: 'Jerry', msg: 'How does the 28th look?'},
+    {from: 'Robert', msg: 'Thank you for reaching out!'},
+  ]
+}
+
+function reducer(state, action) {
+  const { user, from, msg } = action.payload;
+  switch(action.type) {
+    case 'RECEIVE_MESSAGE':
+      return {
+        ...state,
+        [user]: [
+          ...state[user],
+          {
+            from: from,
+            msg: msg
+          }
+        ]
+      }
+    default: 
+      return state
+  }
+}
+
+export default function Store(props) {
+
+  const reducerHook = useReducer(reducer, intialState)
+
   return (
-    <CTX.Provider value={}>
+    <CTX.Provider value={reducerHook}>
       {props.children}
     </CTX.Provider>
   )
 }
-
-export default Store
